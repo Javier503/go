4ccbb1c574580355403046258f3379c11939550d
Javier Flores <javieralexander.flores@lifemiles.com>
2023-12-04 21:55:04 -0600

    siguiente Commit

--- main.go
+++ main.go
@@ -2,23 +2,13 @@ package main
 
 import (
 	"fmt"
-	"runtime"
+
+	"github.com/gocero/go/ejercicios"
 )
 
 func main() {
-	if os := runtime.GOOS; os == "linux" || os == "OS X." {
-		fmt.Println("No es windows es", os)
-	} else {
-		fmt.Println("esto es windows")
-	}
 
-	switch os := runtime.GOOS; os {
-	case "linux":
-		fmt.Println("esto es linux")
-	case "darwin":
-		fmt.Println("esto es darwin")
-	default:
-		fmt.Printf("%s \n", os)
-	}
-
+	numero, texto := ejercicios.Ejercicio("500")
+	fmt.Println(numero)
+	fmt.Println(texto)
 }
